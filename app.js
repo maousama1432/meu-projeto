408bbe99e16fc687f6492a9caf90de9128eebcf3
maousama1432 <adm.felipemr@gmail.com>
2023-10-26 16:08:10 -0400

    atualização

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+  {id:1, Imagem: "public/imagem/frente.jpg", nome: "nome", descricao: "descricao", preco: "2500", classificacao: "informatica"},
+  {id:2, Imagem: "public/imagem/costas.jpg",nome: "nome", descricao: "descricao", preco: "3000", classificacao: "informatica"},
+  {id:3, Imagem: "public/imagem/incluido.jpg",nome: "nome", descricao: "descricao", preco: "7000", classificacao: "informatica"},
+  {id:4, Imagem: "public/imagem/logo.jpg",nome: "nome", descricao: "descricao", preco: "1200", classificacao: "informatica"},
+  {id:5, Imagem: "public/imagem/teclado.jpg",nome: "nome", descricao: "descricao", preco: "2700", classificacao: "informatica"},
+  {id:6, Imagem: "Imagem",nome: "nome", descricao: "descricao", preco: "preco", classificacao: "classificacao"},
+  {id:7, Imagem: "Imagem",nome: "nome", descricao: "descricao", preco: "preco", classificacao: "classificacao"},
+  {id:8, Imagem: "Imagem",nome: "nome", descricao: "descricao", preco: "preco", classificacao: "classificacao"},
+  {id:9, Imagem: "Imagem",nome: "nome", descricao: "descricao", preco: "preco", classificacao: "classificacao"},
+  {id:10, Imagem: "Imagem",nome: "nome", descricao: "descricao", preco: "preco", classificacao: "classificacao"}
+]
+
 app.get('/', (req, res) => {
   res.render('index', { message: 'Olá mundo' });
 });
